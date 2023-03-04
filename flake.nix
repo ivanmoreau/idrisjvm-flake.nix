4f5066945cd55270914b2b0c74a6ec0c8199f1a5
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-03-03 22:00:18 -0600

    misc fixes
    Co-authored-by: Iván Molina Rebolledo <ivan@ivmoreau.com>

--- flake.nix
+++ flake.nix
@@ -14,11 +14,10 @@
         inputs.mission-control.flakeModule
       ];
       perSystem = { self', pkgs, lib, config, system, ... }: {
-        packages.idris2_jvm = pkgs.stdenv.mkDerivation {
+        packages.idris2_jvm = with pkgs; stdenv.mkDerivation {
             name = "idris-jvm";
-            depsBuildBuild = with pkgs; [ wget makeWrapper ];
-            propagatedBuildInputs = with pkgs; [ jdk17_headless ];
-            nativeBuildInputs = with pkgs; [ jdk17_headless ];
+            depsBuildBuild = [ wget makeWrapper ];
+            propagatedNativeBuildInputs = [ jdk17_headless ];
             src = pkgs.fetchzip {
               url = "https://github.com/mmhelloworld/idris-jvm/releases/download/latest/idris2-0.6.0.zip";
               sha256 = "sha256-xJg1EkcLg3y70OhROBOR9yPl4MnluqpeEJpHJf2mnBA=";
@@ -28,7 +27,8 @@
               chmod 0664 "$out/exec/idris2"
               chmod +x "$out/exec/idris2"
               mkdir -p $out/bin
-              makeWrapper $out/exec/idris2 $out/bin/idris2 --set IDRIS_PREFIX=$out/env
+              makeWrapper $out/exec/idris2 $out/bin/idris2 \
+                --set IDRIS2_PREFIX $out/env
             '';
           };
 
